a40cc3dbec57894292ff88c3f08248214e4328f9
¨Mati711¨ <mdavid@udesa.edu.ar>
2023-11-14 23:46:19 -0300

    subo cambios

---
+++ js/details.js
@@ -0,0 +1,65 @@
+let qs = location.search
+let qsobj = new URLSearchParams(qs);
+
+let type = qsobj.get('type')
+
+
+
+let section = document.querySelector('.containerDetail')
+
+if (type == "pelicula") {
+    let idpelicula = qsobj.get('id')
+    let urldetail = `https://api.themoviedb.org/3/movie/${idpelicula}?api_key=dd90d0acdb090a890837077054c376e1&language=en-US`
+        fetch(urldetail)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        let contenido = `<h2>${data.title}</h2>
+        <img src="https://image.tmdb.org/t/p/w500/${data.backdrop_path}">
+        <p> <b>Estreno: </b>${data.release_date}</p>
+        <p> <b>Duración: </b> ${data.runtime} minutes</p>
+        <p>Generos:  ${data.genres[0].name}</p>
+        <p>Sinopsis: ${data.overview}</p>`
+
+        section.innerHTML = contenido;
+
+
+    
+        return data;
+    })
+    .catch(function(error) {
+        return error;
+    });
+        
+}
+
+
+if (type == "serie") {
+    let idserie = qsobj.get('id')
+    let urlser = `https://api.themoviedb.org/3/tv/${idserie}?api_key=dd90d0acdb090a890837077054c376e1&language=en-US`
+        fetch(urlser)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        let contenido = `<h2>${data.name}</h2>
+        <img src="https://image.tmdb.org/t/p/w500/${data.backdrop_path}">
+        <p> <b>Estreno: </b>${data.first_air_date}</p>
+        <p> <b>Duración: </b> ${data.number_of_seasons} seasons</p>
+        <p>Generos:  ${data.genres[0].name} </p>
+        <p>Sinopsis: ${data.overview}</p>`
+
+        section.innerHTML = contenido;
+
+
+    
+        return data;
+    })
+    .catch(function(error) {
+        return error;
+    });
+        
+}
--- js/index.js
+++ js/index.js
@@ -1,31 +1,94 @@
-let apiKey = "4be5afe5e53cd4b3126a5264e7bc79a6"
-fetch(`=${apiKey}`)
-.then(function(response){
-    return response.json()
-})
+api_key = "dd90d0acdb090a890837077054c376e1"
+let urlUserpopular = `https://api.themoviedb.org/3/discover/movie?api_key=dd90d0acdb090a890837077054c376e1&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`
+let urlUsertoprated = `https://api.themoviedb.org/3/discover/movie?api_key=dd90d0acdb090a890837077054c376e1&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200`
+let urlseriespopu = `https://api.themoviedb.org/3/discover/tv?api_key=dd90d0acdb090a890837077054c376e1&include_adult=false&language=en-US&page=1&sort_by=popularity.desc`
 
-let container= document.querySelector('.pelis')
-fetch ('https://rickandmortyapi.com/api/character ') 
-
-.then(function(response){
-    console.log(response)
-    return response.json() 
-
+let sectionpopular = document.querySelector('.container1')
+fetch(urlUserpopular)
+.then(function(response) {
+    return response.json();
 })
 .then(function(data) {
-    for(let i=0; i < data.results.length; i++ ){
-    container.innerHTML += `
-    <article> 
-        <h2>${data.results[i].name}</h2>
-        <img src=${data.results[i].image}>
+    console.log(data)
+    let result = data.results;
+    let contenido = "";
+
+    for (let i = 0; i < 5; i++) {
+        contenido += `<article class="peli1">
+        <a href="./detail-movie.html?id=${result[i].id}&type=pelicula"><h3>${result[i].title}</h3></a>
+        <img src="https://image.tmdb.org/t/p/w500/${result[i].poster_path}" border="6">
+        <p>Fecha de estreno:${result[i].release_date}</p>
     </article>`
-}
+    }
 
-})
-.catch(function(error){
-    console.log(error)
-})
+    sectionpopular.innerHTML = contenido;
 
+
+  
+    return data;
+})
+.catch(function(error) {
+    return error;
+});
+
+
+
+let sectiontoprated = document.querySelector('.container2')
+fetch(urlUsertoprated)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data)
+    let result = data.results;
+    let contenido = "";
+
+    for (let i = 0; i < 5; i++) {
+        contenido += `<article class="peli1">
+        <a href="./detail-movie.html?id=${result[i].id}&type=pelicula"><h3>${result[i].title}</h3></a>
+        <img src="https://image.tmdb.org/t/p/w500/${result[i].poster_path}" border="6">
+        <p>Fecha de estreno:${result[i].release_date}</p>
+    </article>`
+    }
+
+    sectiontoprated.innerHTML = contenido;
+
+
+  
+    return data;
+})
+.catch(function(error) {
+    return error;
+});
+
+
+let sectionupcoming = document.querySelector('.container3')
+fetch(urlseriespopu)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data)
+    let result = data.results;
+    let contenido = "";
+
+    for (let i = 0; i < 5; i++) {
+        contenido += `<article class="peli1">
+        <a href="./detail-movie.html?id=${result[i].id}&type=serie"><h3>${result[i].name}</h3></a>
+        <img src="https://image.tmdb.org/t/p/w500/${result[i].poster_path}" border="6">
+        <p>Fecha de estreno:${result[i].first_air_date}</p>
+    </article>`
+    }
+
+    sectionupcoming.innerHTML = contenido;
+
+
+  
+    return data;
+})
+.catch(function(error) {
+    return error;
+});
 
 
 
